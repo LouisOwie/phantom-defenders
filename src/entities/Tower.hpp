c0bfd90fd10939774c9066ad7673c95b90812236
LouisOwie <louisowie@gmx.de>
2025-11-27 22:44:21 +0100

    Add Tower class implementation and refactor MyApplication to utilize it

---
+++ src/entities/Tower.hpp
@@ -0,0 +1,19 @@
+﻿#ifndef PHANTOM_DEFENDERS_TOWER_HPP
+#define PHANTOM_DEFENDERS_TOWER_HPP
+#include <GL/glew.h>
+#include "utils/Shader.hpp"
+
+class Tower {
+ public:
+    Tower(ShaderProgram& shaderProgram);
+    void draw(glm::mat4 projection, glm::mat4 view);
+
+ private:
+    const int size = 100;
+    ShaderProgram& shaderProgram;
+    // VBO/VAO/ibo
+    GLuint vao, vbo, ibo;
+
+    void createObject();
+};
+#endif //PHANTOM_DEFENDERS_TOWER_HPP
--- src/window/MyApplication.hpp
+++ src/window/MyApplication.hpp
@@ -11,28 +11,29 @@
 
 #include "Application.hpp"
 #include "../utils/Shader.hpp"
+#include "../entities/Tower.hpp"
+#include <vector>
 
 class MyApplication : public Application {
- public:
-  MyApplication();
+public:
+    MyApplication();
 
- protected:
-  virtual void loop();
+protected:
+    virtual void loop();
 
- private:
-  const int size = 100;
+private:
+    const int size = 100;
 
-  // shader
-  Shader vertexShader;
-  Shader fragmentShader;
-  ShaderProgram shaderProgram;
+    std::vector<Tower> entities;
 
-  // shader matrix uniform
-  glm::mat4 projection = glm::mat4(1.0);
-  glm::mat4 view = glm::mat4(1.0);
+    // shader
+    Shader vertexShader;
+    Shader fragmentShader;
+    ShaderProgram shaderProgram;
 
-  // VBO/VAO/ibo
-  GLuint vao, vbo, ibo;
+    // shader matrix uniform
+    glm::mat4 projection = glm::mat4(1.0);
+    glm::mat4 view = glm::mat4(1.0);
 };
 
 #endif  // OPENGL_CMAKE_SKELETON_MYAPPLICATION
