652ce7340413841b4a6d02b8189bd03953ca5ef3
LouisOwie <louisowie@gmx.de>
2025-12-01 22:26:15 +0100

    Add Camera class implementation and update CMakeLists for new files

---
+++ src/window/Camera.cpp
@@ -0,0 +1,13 @@
+﻿#include "Camera.hpp"
+#include <glm/gtc/matrix_transform.hpp>
+
+Camera::Camera(glm::vec3 position, glm::vec3 target, glm::vec3 up = glm::vec3(0,0,1), float fov = 90.0f, float nearClip = 0.1f, float farClip = 100.0f)
+    : position(position), target(target), up(up), fov(fov), nearClip(nearClip), farClip(farClip) {}
+
+glm::mat4 Camera::getViewMatrix() {
+    return glm::lookAt(position, target, up);
+}
+
+glm::mat4 Camera::getProjectionMatrix(float aspect) {
+    return glm::perspective(glm::radians(fov), aspect, nearClip, farClip);
+}
---
+++ src/window/Camera.hpp
@@ -0,0 +1,19 @@
+﻿#ifndef PHANTOM_DEFENDERS_CAMERA_HPP
+#define PHANTOM_DEFENDERS_CAMERA_HPP
+#include <glm/glm.hpp>
+
+class Camera {
+public:
+    Camera(glm::vec3 position, glm::vec3 target, glm::vec3 up, float fov, float nearClip, float farClip);
+    glm::mat4 getViewMatrix();
+    glm::mat4 getProjectionMatrix(float aspect);
+private:
+    glm::vec3 position;
+    glm::vec3 target;
+    glm::vec3 up;
+
+    float fov;
+    float nearClip;
+    float farClip;
+};
+#endif //PHANTOM_DEFENDERS_CAMERA_HPP
